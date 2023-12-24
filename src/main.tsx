67bd7e5c97ce15faba663a18bc5d6b84a1a3f86c
Cass <bikbaevf18@gmail.com>
2023-12-24 14:22:26 +0400

    Init styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
-import './index.scss';
+import App from '@/App.tsx';
+
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
