401244410692a27ee49447778455cb7e8ed24745
Cass <bikbaevf18@gmail.com>
2023-12-28 00:36:06 +0400

    Update Sign component

--- src/components/Sign/SignToastMessage.tsx
+++ src/components/Sign/SignToastMessage.tsx
@@ -3,15 +3,17 @@ import styles from './Sign.module.scss';
 export default function SignToastMessage() {
   return (
     <div className={styles.message}>
-      <p>Разработал</p>
-      <div className={styles.circle}></div>
-
+      <p>Developed by</p>
+      <div className={styles.logo}>
+        <img src='/icons/fyargat.svg' alt='Fyargat Logo Icon' />
+      </div>
       <a
+        className={styles.link}
         href='https://fyargat.com/'
         target='_blank'
         rel='nofollow noopener noreferrer'
       >
-        Фяргать Бикбаев
+        <span>Fyargat Bikbaev</span>
       </a>
     </div>
   );
